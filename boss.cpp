94ec0eea3351316b508f622a639795a468c22435
Redcat666-cj <1851713370@qq.com>
2026-02-10 23:05:39 +0800

    Create boss.cpp

---
+++ boss.cpp
@@ -0,0 +1,27 @@
+#include "boss.h"
+
+using namespace std;
+
+
+Boss::Boss(int id, string name, int did) {
+
+	this->m_Id = id;
+	this->m_Name = name;
+	this->m_Part = did;
+
+}
+void Boss::showInfo() {
+
+	cout << "职工编号：" << this->m_Id
+		<< "\t职工姓名：" << this->m_Name
+		<< "\t岗位：" << this->getPart()
+		<< "\t部门编号：" << this->m_Part
+		<< endl;
+
+
+}
+string Boss::getPart() {
+
+	return "老板";
+
+}
